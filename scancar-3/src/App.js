7b38e5e47bf62b200c2a8eaf29b7f589ff8899c8
lpjc <Lars.p.j.c@gmail.com>
2021-10-31 11:55:09 +0100

    Added search function
    Table search function added. Search through the whole table in one field.
    Minor improvements to layout.
    Cleaned dataForTable.js a little.

--- scancar-3/src/App.js
+++ scancar-3/src/App.js
@@ -1,8 +1,52 @@
 import React from 'react'
-import { useTable } from 'react-table'
+import { useTable, useGlobalFilter, useAsyncDebounce } from 'react-table'
 import makeData from './dataForTable'
 
-function App() {
+/*
+
+V1.0
+Creating a table in our App()
+lacl@itu.dk 30/oct/2021
+
+V1.1
+Added search global functionality for table and made minor layout adjustment
+lacl@itu.dk 31/oct/2021
+
+*/
+
+function GlobalFilter({ //function that can be put into a table, to add a functioning search bar.
+  preGlobalFilteredRows,
+  globalFilter,
+  setGlobalFilter,
+}) {
+  const count = preGlobalFilteredRows.length
+  const [value, setValue] = React.useState(globalFilter)
+  const onChange = useAsyncDebounce(value => {
+    setGlobalFilter(value || undefined)
+  }, 200)
+
+  return (
+    <span>
+      Search:{' '}
+      <input
+        value={value || ""}
+        onChange={e => {
+          setValue(e.target.value);
+          onChange(e.target.value);
+        }}
+        placeholder={`${count} upcoming rentals...`}
+        style={{
+          fontSize: '1.1rem',
+          border: '0',
+        }}
+      />
+    </span>
+  )
+}
+
+
+
+function App() { // our table function should soon be seperated from our App()
  
   const data = React.useMemo(() => makeData(30), [])
 
@@ -16,38 +60,52 @@ function App() {
     ],[]
   ) 
   
-  const tableInstance = useTable({ columns, data })
+  const rentalTable = useTable({ columns, data }, useGlobalFilter)
   // with this we can now hook onto a table that utilize our colums and data
+
   const { 
     getTableProps,
     getTableBodyProps,
     headerGroups,
     rows,
     prepareRow,
-  } = tableInstance 
+    preGlobalFilteredRows,
+    state,
+    setGlobalFilter,
+  } = rentalTable 
   // now we can use the react-table functions!
 
   return (
-    <table {...getTableProps()} style={{ border: 'solid 1px blue' }}>
+    <table {...getTableProps()} style={{ 
+      border: 'solid 10px #F7E8A4'}}>
       <thead>
+        
+        <GlobalFilter
+          preGlobalFilteredRows={preGlobalFilteredRows}
+          globalFilter={state.globalFilter}
+          setGlobalFilter={setGlobalFilter}
+        />
+
         {headerGroups.map(headerGroup => (
           <tr {...headerGroup.getHeaderGroupProps()}>
             {headerGroup.headers.map(column => (
               <th
                 {...column.getHeaderProps()}
                 style={{
-                  borderBottom: 'solid 3px red',
-                  background: 'aliceblue',
+                 
+                  borderBottom: 'solid 3px black',
+                  background: '#F7E8A4',
                   color: 'black',
                   fontWeight: 'bold',
                 }}
-              >
+              > 
                 {column.render('Header')}
               </th>
             ))}
           </tr>
         ))}
       </thead>
+      
       <tbody {...getTableBodyProps()}>
         {rows.map(row => {
           prepareRow(row)
@@ -58,9 +116,9 @@ function App() {
                   <td
                     {...cell.getCellProps()}
                     style={{
-                      padding: '10px',
+                      padding: '15px',
                       border: 'solid 1px gray',
-                      background: 'papayawhip',
+                      background: '#FBF3D0',
                     }}
                   >
                     {cell.render('Cell')}
--- scancar-3/src/dataForTable.js
+++ scancar-3/src/dataForTable.js
@@ -6,22 +6,29 @@ let DateGenerator = require('random-date-generator');
 
 /*
 
+V1.0
 Randomly generating table data based react-table examples
-
 lacl@itu.dk 30/oct/2021
+
+V1.1
+Cleaned the code up a little, added functionality randomFromArrray()
+lacl@itu.dk 31/oct/2021
 
 */ 
 
 
+function randomFromArray(array) {
+  return array[Math.floor(Math.random()*array.length)]
+}
 
-const carGroupArr=['A','B','C','D','E']
+const carGroups=['A','B','C','D','E']
 
-const minutesArr=['00','15','30','45']
+const minuteIntervals=['00','15','30','45']
 
 //Create random, formatted, dated. Maybe needs logic to be increasing all the time?
 function randomizeDate(){
     const randomDate = DateGenerator.getRandomDateInRange(new Date(),new Date(2021,11,0));
-    const formattedDate = format(randomDate, 'MMM do HH:'+ minutesArr[Math.floor(Math.random()*minutesArr.length)])
+    const formattedDate = format(randomDate, 'MMM do HH:'+ randomFromArray(minuteIntervals))
     return formattedDate
 }
 
@@ -37,7 +44,7 @@ const newRental = () =>{
         bookingID: Math.floor(Math.random()*10000000),
         firstName: uniqueNamesGenerator(nameSettings),
         lastName: namor.generate({words: 1, saltLength: 0}),
-        carGroup: carGroupArr[Math.floor(Math.random()*carGroupArr.length)]
+        carGroup: randomFromArray(carGroups)
     }
 
 }
