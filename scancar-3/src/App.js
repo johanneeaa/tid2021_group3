8bb4d0ff50ce58efbff562a8324aad16aacda0b1
lpjc <Lars.p.j.c@gmail.com>
2021-11-05 19:20:12 +0100

    Component folder added, GlobalFilter moved

--- scancar-3/src/App.js
+++ scancar-3/src/App.js
@@ -1,41 +1,15 @@
 import React from 'react'
 import { useTable, useGlobalFilter, useAsyncDebounce } from 'react-table' //React table documentaion https://react-table.tanstack.com/
 import makeData from './dataForTable'
+import { GlobalFilter } from './Components/GlobalFilter'
+import { TableRow } from './Components/TableRow'
+
 
 /*
 
 */
 
-function GlobalFilter({ //component that can be put into a table, to add a functioning search bar.
-  preGlobalFilteredRows,
-  globalFilter,
-  setGlobalFilter,
-}) {
-  const count = preGlobalFilteredRows.length
-  const [value, setValue] = React.useState(globalFilter)
-  const onChange = useAsyncDebounce(value => {
-    setGlobalFilter(value || undefined)
-  }, 200)
 
-  return (
-    <span>
-      Search:{' '}
-      <input
-        value={value || ""}
-        onChange={e => {
-          setValue(e.target.value);
-          onChange(e.target.value);
-        }}
-        placeholder={`${count} upcoming rentals...`}
-        style={{
-          fontSize: '1.1rem',
-          border: '1',
-          width: 350,
-        }}
-      />
-    </span>
-  )
-}
 function onRowClick(row) {
   console.log(row.original);
   console.log(row.style);
@@ -77,67 +51,75 @@ function App() { // our table component should soon be seperated from our App()
   // now we can use the react-table functions!
 
   return (
-    
-    <table {...getTableProps()} style={{ 
-      border: 'solid 10px #F7E8A4',
-      }}
-      >        
-      <thead>
-        <th colSpan={visibleColumns.length}
-          style={{
-          textAlign: 'left',
-        }}>
-          <GlobalFilter
-            preGlobalFilteredRows={preGlobalFilteredRows}
-            globalFilter={state.globalFilter}
-            setGlobalFilter={setGlobalFilter}
-          />
-        </th>
+    <div>
+     
+      <table {...getTableProps()} style={{ 
+        border: 'solid 10px #F7E8A4',
+        }}
+        >        
+        <thead>
+          <th colSpan={visibleColumns.length}
+            style={{
+            textAlign: 'left',
+          }}>
+            
+            <GlobalFilter
+              preGlobalFilteredRows={preGlobalFilteredRows}
+              globalFilter={state.globalFilter}
+              setGlobalFilter={setGlobalFilter}
+            />
+            
+          </th>
 
-        {headerGroups.map(headerGroup => (
-          <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map(column => (
-              <th
-                {...column.getHeaderProps()}
-                style={{
-                 
-                  borderBottom: 'solid 30px black',
-                  background: '#F7E8A4',
-                  color: 'black',
-                  fontWeight: 'bold',
-                }}
-              > 
-                {column.render('Header')}
-              </th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      
-      <tbody {...getTableBodyProps()}>
-        {rows.map(row => {
-          prepareRow(row)
-          return (
-            <tr {...row.getRowProps() } style={rowStyle} onClick={() => onRowClick(row)}>
-              {row.cells.map(cell => {
-                return (
-                  <td
-                    {...cell.getCellProps()}
-                    style={{
-                      padding: '15px',
-                      border: 'solid 1px gray',
-                      
-                    }}
-                  >
-                    {cell.render('Cell')}
-                  </td>
-                )
-              })}
+          {headerGroups.map(headerGroup => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map(column => (
+                <th
+                  {...column.getHeaderProps()}
+                  style={{
+                  
+                    borderBottom: 'solid 30px black',
+                    background: '#F7E8A4',
+                    color: 'black',
+                    fontWeight: 'bold',
+                  }}
+                > 
+                  {column.render('Header')}
+                </th>
+              ))}
             </tr>
-          )
-        })}
-      </tbody>
-    </table>
+          ))}
+        </thead>
+
+      
+      
+        <TableRow name = "Ogens"/>
+        <tbody {...getTableBodyProps()}>
+          {rows.map(row => {
+            prepareRow(row)
+            return (
+              <tr {...row.getRowProps() } style={rowStyle} onClick={() => onRowClick(row)}>
+                {row.cells.map(cell => {
+                  return (
+                    <td
+                      {...cell.getCellProps()}
+                      style={{
+                        padding: '15px',
+                        border: 'solid 1px gray',
+                        
+                      }}
+                    >
+                      {cell.render('Cell')}
+                    </td>
+                  )
+                })}
+              </tr>
+            )
+          })}
+        </tbody>
+        
+      </table>
+    </div>
   )
 }
 export default App
