464231482d1058e86198dc017e515675f868cee7
Teresa Møller <term@itu.dk>
2021-11-18 11:03:48 +0100

    adding-template-booking-details

--- src/Components/PopUp.js
+++ src/Components/PopUp.js
@@ -7,9 +7,31 @@ function PopUp(props) {
         return (props.trigger) ? (      //if the trigger is 'true' then the popUp will show, if false it will not
             <div className="popup">
                 <div className="popup-inner">
-                    <br></br>
-                    <main><b>Hi, I'm a PopUp!</b></main>
-                    <br></br>
+                    <div className="popup-info">
+                        <main><b>Booking Details</b></main>
+                        <input placeholder="Booking ID"/>
+                        <br></br>
+                        <input placeholder="First Name"/>
+                        <input placeholder="Last Name"/>
+                        <br></br>
+                        <input placeholder="Driver's license"/>
+                        <input placeholder="Date of Birth"/>
+                        <br></br><br></br>
+                        <input placeholder="Requested Car Group"/>
+                        <input placeholder="Appointed Car i.e. license plate number"/>
+                        <br></br><br></br>
+                        <input placeholder="Pick Up Time"/>
+                        <input placeholder="Pick Up Office"/>
+                        <br></br>
+                        <input placeholder="Mileage"/>
+                        <input placeholder="Fuel Level"/>
+                        <br></br><br></br>
+                        <input placeholder="Return Time"/>
+                        <input placeholder="Return Office"/>
+                        <br></br>
+                        <input placeholder="Mileage"/>
+                        <input placeholder="Fuel Level"/>
+                    </div>
                     <button className="close-button" onClick={() => props.setTrigger(false)}>X</button>
                     {props.children}
                 </div>
