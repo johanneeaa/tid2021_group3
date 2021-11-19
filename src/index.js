99cf6e865e5a3a062e556cbe1c2defede9de8abd
PFisk <fisker.psf@gmail.com>
2021-11-19 12:06:13 +0100

    Initial database integration

--- src/index.js
+++ src/index.js
@@ -4,6 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import Parse from "parse";
+
+Parse.initialize(
+  "AgzfzDrOFClmY4jztxiZnNa3dlEN8fbiDAsWV6lf",
+  "SsjNuDrQGMddrkRrL3vV5tYUYhZQf7RrTTwHHngB"
+);
+
+Parse.serverURL = "https://parseapi.back4app.com/";
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
