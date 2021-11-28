b29cc443cd130b1ee4215345dc7b980e9314f51f
lpjc <Lars.p.j.c@gmail.com>
2021-11-28 10:10:18 +0100

    Functional & basic styling
    The popup is now generic/dynamic and works for all our tables.
    It takes the "nice" headers instead of column.accesors, and populates the components accordingly with number of cells and information. Great succes. Very nice. xoxo

--- src/Components/TextBox4Info.js
+++ src/Components/TextBox4Info.js
@@ -1,23 +1,19 @@
-import { style } from "dom-helpers"
-import { useState } from "react"
-import { Placeholder } from "react-bootstrap"
+// Textbox component. Takes a title and info, if there is info, the input will "lock".  
+// Needs styling as of 28/nov/2021 @lacl
 
 export default function Textbox4Info(props){
-
 
         return (
             <div>
             <h3>{props.title}</h3>
             <div className="inputBox">
-            <input 
-                placeholder = "..."        
-                value = {props.info} 
-                // Value will overwrite placeholder if props.info == true
-            /> 
+                <input 
+                    placeholder = "..."        
+                    value = {props.info} 
+                    // Value will overwrite placeholder if props.info == true
+                /> 
             </div>
-            
             </div>
         )
-
 
 }
