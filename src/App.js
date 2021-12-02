10ac293083a769abadc2d54bf058ba1fd134dc10
Teresa Møller <term@itu.dk>
2021-12-02 15:06:28 +0100

    minor changes to app

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter, Routes, Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { NavigationBar } from "./Components/NavigationBar";
 import Cars from "./Pages/Cars";
 import Rental from "./Pages/Rental";
-
-
 // our App(), should containt our navigation with paths to pages
 // Tab navigation needs styling, we can't see what are pressed
-
-function App() { 
+function App() {
+  const temp = (
+    <main>
+      <b>Temporary front page. Use tabs to access data</b>
+    </main>
+  );
   return (
     <>
-    <NavigationBar /> 
-
+      <NavigationBar />
       <BrowserRouter>
         <Routes>
+          <Route exact path="/" element={temp} />
           <Route path="/cars" element={<Cars />} />
-          <Route path="/rental" element={<Rental /> } />
+          <Route path="/rental" element={<Rental />} />
         </Routes>
       </BrowserRouter>
-    Temporary front page. Use tabs to acces data 
-  </>
+    </>
   );
 }
-
 export default App;
