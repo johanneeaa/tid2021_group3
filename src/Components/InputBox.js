84d8ee2ef9e576b6052fbdee1ac8dd416a0d1ee6
Teresa Møller <term@itu.dk>
2021-12-08 16:13:57 +0100

    minor cleanup inputbox

--- src/Components/InputBox.js
+++ src/Components/InputBox.js
@@ -1,20 +1,14 @@
-import { useState } from 'react';
-import deleteCostumerByID from '../Functions/DeleteCostumer';
+import { useState } from "react";
+import deleteCostumerByID from "../Functions/DeleteCostumer";
 
 export default function InputBox(props) {
+  const [input, setInput] = useState("");
 
-    const [input, setInput] = useState('');
-
-    function removeAndRefresh(input) {
-        deleteCostumerByID(input)
-        window.location.reload(false)
-    }
-
-    return (
-        <div>
-            <label>Enter costumer ID for deletion: </label>
-            <input value={input} onInput={e => setInput(e.target.value)}/>
-            <button onClick={()=>deleteCostumerByID(input)}> Remove the costumer with this ID </button> 
-        </div>
-    );
+  return (
+    <div>
+      <label>Enter costumer ID for deletion: </label>
+      <input value={input} onInput={(e) => setInput(e.target.value)} />
+      <button onClick={() => deleteCostumerByID(input)}> Remove the costumer with this ID </button>
+    </div>
+  );
 }
