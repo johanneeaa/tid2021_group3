4377996e3dd9cbdf63a0a8a03bec25c353fb83b6
Johanne Engel Aaen <jaae@itu.dk>
2021-12-08 20:26:12 +0100

    initial css added

--- src/Components/NavigationBar.js
+++ src/Components/NavigationBar.js
@@ -1,19 +1,24 @@
 import { Navbar, Container, Nav } from "react-bootstrap";
+import './NavigationBar.css'
 
 export function NavigationBar() {
     return (
-        <Navbar bg="light" expand="lg">
-            <Container>
-                <Navbar.Brand href="/">ScanCar</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
+     
+        <Navbar className = "navbar">
+            <Container href="container">
+              <Navbar.Brand href = "home" className = "logo">ScanCar</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="Rental">Rental</Nav.Link>
-                        <Nav.Link href="Cars">Cars</Nav.Link>
-                        <Nav.Link href="Statistics">Statistics</Nav.Link>
+                            <Nav.Link className="rentallink" href="Rental">Rental</Nav.Link>
+                            <Nav.Link className="carslink" href="Cars">Cars</Nav.Link>
+                            <Nav.Link className="customerslink" href = "Customers">Customers</Nav.Link>
+                            <Nav.Link className="statisticslink" href="Statistics">Statistics</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
         </Navbar>
+    
     );
+
 }
