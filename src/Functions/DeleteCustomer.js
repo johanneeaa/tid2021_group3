c86f69f5e7c489321dfa4a736c972188a6a47399
Philip <phfi@itu.dk>
2021-12-11 22:45:43 +0100

    Fetching of key from .env

--- src/Functions/DeleteCustomer.js
+++ src/Functions/DeleteCustomer.js
@@ -1,6 +1,9 @@
 // Deleting a Customer based off an input in the form of an ID
 // built on example from TID and [ https://dashboard.back4app.com/apidocs/ ]
 
+const APP_ID_KEY = process.env.REACT_APP_APP_KEY
+const APP_JS_KEY = process.env.REACT_APP_JS_KEY
+
 export default async function deleteCustomerByID(ID) {
   try {
     const response = await fetch(
@@ -8,8 +11,8 @@ export default async function deleteCustomerByID(ID) {
       {
         method: "DELETE",
         headers: {
-          "X-Parse-Application-Id": "", //INSERT APPLICATION ID HERE
-          "X-Parse-REST-API-Key": "", //INSERT APP JS KEY HERE
+          "X-Parse-Application-Id": APP_ID_KEY, //Getting key from .env
+          "X-Parse-REST-API-Key": APP_JS_KEY, //Getting key from .env
         },
       }
     );
