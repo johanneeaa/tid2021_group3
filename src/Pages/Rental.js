6f99a27fab038a507bda45921a34cad6b0965ecd
Teresa Møller <term@itu.dk>
2021-12-23 14:32:04 +0100

    formatting to prettier

--- src/Pages/Rental.js
+++ src/Pages/Rental.js
@@ -1,9 +1,9 @@
-import React, { useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import getAllBookings from "../Data/rentalData";
 import Table from "../Components/Table";
 import SelectColumnFilter from "../Components/Filters";
 //import createNewBooking from "../Functions/NewBooking";
-import './Rental.css';
+import "./Rental.css";
 
 const RentalTable = () => {
   const [rentalData, setRentalData] = useState([]);
@@ -11,16 +11,28 @@ const RentalTable = () => {
   const rentalColumns = React.useMemo(
     () => [
       { Header: "Booking Number", accessor: "bookingNumber" },
-      { Header: "Pick Up Office", accessor: "pickUpOffice", localFilter: true, disableGlobalFilter: true, Filter: SelectColumnFilter},
-      { Header: "Pick Up Date", accessor: "pickUpDate"},
-      { Header: "Pick Up Time", accessor: "pickUpTime"},
-      { Header: "Return Office", accessor: "returnOffice", localFilter: true, disableGlobalFilter: true, Filter: SelectColumnFilter},
-      { Header: "Return Date", accessor: "returnDate"},
-      { Header: "Return Time", accessor: "returnTime"},
+      {
+        Header: "Pick Up Office",
+        accessor: "pickUpOffice",
+        localFilter: true,
+        disableGlobalFilter: true,
+        Filter: SelectColumnFilter,
+      },
+      { Header: "Pick Up Date", accessor: "pickUpDate" },
+      { Header: "Pick Up Time", accessor: "pickUpTime" },
+      {
+        Header: "Return Office",
+        accessor: "returnOffice",
+        localFilter: true,
+        disableGlobalFilter: true,
+        Filter: SelectColumnFilter,
+      },
+      { Header: "Return Date", accessor: "returnDate" },
+      { Header: "Return Time", accessor: "returnTime" },
       { Header: "Customer Name", accessor: "fullName" },
       { Header: "Date of Birth", accessor: "dateOfBirth" },
       { Header: "Drivers License", accessor: "driversLicense" },
-      { Header: "Requested Car Group", accessor: "reqCarGroup"},
+      { Header: "Requested Car Group", accessor: "reqCarGroup" },
     ],
     []
   );
@@ -31,15 +43,26 @@ const RentalTable = () => {
       setRentalData(rentalDataTemp);
     }
     fetchData();
-  },[])
+  }, []);
 
-  return     <div>
-  <div className = "bookingcontainer">
-    <br/>
-    <button className = "createNewBookingButton" /* onClick={window.location.href = '/newbooking'} return false */> Create new Booking</button><br/>
+  return (
+    <div>
+      <div className="bookingcontainer">
+        <br />
+      <div> 
+        <button
+          className="createNewBookingButton"
+          //onClick={/* (window.location.href = "/newbooking") */} //this is making problems - maybe we can make a button component instead
+        >
+          {" "}
+          Create new Booking
+        </button>
+        <br />
+      </div></div> 
+      <br />
+      <Table columns={rentalColumns} data={rentalData} />
     </div>
-    <br/><Table columns={rentalColumns} data={rentalData}/>
-    </div>;
+  );
 };
 
 export default RentalTable;
