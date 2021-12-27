aa6eaeb82af195bebde6f25264bcfde8d4077a32
Teresa Møller <term@itu.dk>
2021-12-27 15:27:21 +0100

    adding an alert and button

--- src/Pages/NewBooking.js
+++ src/Pages/NewBooking.js
@@ -4,9 +4,10 @@ import CustomerForm from "../Components/CustomerForm";
 function CreateBooking() {
   return (
     <div>
+      <button className = "returnButton" onClick={() => { window.location.href = '/rental'}} > Return to Rental overview</button><br/>
       <BookingForm>Create New Booking</BookingForm>
       <CustomerForm>Create New Customer</CustomerForm>
-      {/* <button className = "cancelButton" onClick={window.location.href = '/rental'} > Return to Rental overview</button><br/> */}
+      
     </div>
   );
 }
