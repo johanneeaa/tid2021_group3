67e0bcbaa686e1d7c378188981553b95ccaf8e82
Teresa Møller <term@itu.dk>
2021-12-27 15:25:01 +0100

    fixed rentaltable with new database columns

--- src/Data/rentalData.js
+++ src/Data/rentalData.js
@@ -7,34 +7,19 @@ export default async function getAllBookings() {
   const allBookings = await allBookingsQuery.find();
 
   const allBookingsFormatted = allBookings.map((booking) => {
-    const pickUpDate = new Date(booking.get("PickUpTime"))
-      .toISOString()
-      .slice(5, 10); // for hours and minutes (slicing on characters from index 0 - 10)
-    const pickUpTime = new Date(booking.get("PickUpTime"))
-      .toISOString()
-      .slice(11, 16); // for hours and minutes (slicing on characters from index 0 - 10)
-
-    const returnDate = new Date(booking.get("ReturnTime"))
-      .toISOString()
-      .slice(5, 10);
-    const returnTime = new Date(booking.get("ReturnTime"))
-      .toISOString()
-      .slice(11, 16);
-
-    const dob = new Date(booking.get("DoB")).toISOString().slice(0, 10);
 
     return {
       id: booking.id,
       pickUpOffice: booking.get("PickUpOffice"),
-      pickUpDate: pickUpDate,
-      pickUpTime: pickUpTime,
+      pickUpDate: booking.get("PickUpDate"),
+      pickUpTime: booking.get("PickUpTime2"),
       returnOffice: booking.get("ReturnOffice"),
-      returnDate: returnDate,
-      returnTime: returnTime,
+      returnDate: booking.get("ReturnDate"),
+      returnTime: booking.get("ReturnTime2"),
       bookingNumber: booking.get("BookingID"),
       fullName: booking.get("LastName") + ", " + booking.get("FirstName"),
-      dateOfBirth: dob,
-      driversLicense: booking.get("DriversLicense"),
+      dateOfBirth: booking.get("DoB2"),
+      driversLicense: booking.get("DLicense"),
       reqCarGroup: booking.get("ReqCarGroup"),
     };
   });
