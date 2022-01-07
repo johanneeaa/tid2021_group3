65b6fd9c582dcbe1dcd6171aa847ab07034489a6
lpjc <Lars.p.j.c@gmail.com>
2022-01-07 08:14:55 +0100

    Update Loadscreen.js
    Added wait time for clarity

--- src/Components/Loadscreen.js
+++ src/Components/Loadscreen.js
@@ -6,7 +6,7 @@ const Loadscreen = () => {
   return (
     <div style={{display: "flex", alignItems: "center", flexDirection: "column"}}>
         <img src="https://pakhuset-odder.dk/oh_booking_frontend/static/img/loading.gif" alt="Loading" style={{ align: 'center' }}/>   
-        <h1>Fetching data...</h1>
+        <h3>Fetching data, wait time ~ 10 sec</h3>
     </div>
   );
 };
